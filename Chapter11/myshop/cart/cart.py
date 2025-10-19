d153405f2b36992a5a4b4222b6972454c78202a8
Ali Amini <80108336+aliamini-gh@users.noreply.github.com>
2025-10-19 23:09:09 +0330

    chore: update comment

--- Chapter11/myshop/cart/cart.py
+++ Chapter11/myshop/cart/cart.py
@@ -73,7 +73,8 @@ class Cart:
     def clear(self):
         # remove cart from session
         del self.session[settings.CART_SESSION_ID]
-        # remove coupon_id from session
+
+        # remove used coupon from session
         del self.session['coupon_id']
         self.save()
 
